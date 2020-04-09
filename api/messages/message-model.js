76f43b9089cc3d0177c587b17fb3037ef87e0db9
ElkBranch <elggren.spencer@gmail.com>
2020-04-09 16:43:20 -0600

    Uncommented

--- api/messages/message-model.js
+++ api/messages/message-model.js
@@ -9,12 +9,12 @@ module.exports = {
 
 function getEmailList(query) {
   return db("emails")
+      .limit(query.limit)
+      .offset(query.skip)
       .orderBy('date')
       .select('message_id', 'from',
           'subject', 'date', 'email_body_text')
       .substring(1, 50)
-      .limit(query.limit)
-      .offset(query.skip)
 }
 
 function getEmail(id) {
@@ -28,119 +28,119 @@ function getThreadList(threadID) {
       .orderBy('date')
       .where('gmThreadID', threadID)
 }
-// module.exports = {
-//   getResults,
-//   getLastEmailFromUser,
-//   getEmailIds,
-//   addEmail,
-//   deleteAllEmailsByUser,
-//   updateEmail,
-//   deleteEmail,
-//   getTagsForMessage,
-//   getMessageTagsFromUser,
-//   get,
-//   findEmailbyId,
-//   emails
-// };
-//
-// function getResults(userId, results) {
-//   // const numArray = results.map(num => {
-//   //   return num * 1;
-//   // });
-//
-//   return db("emails")
-//     .whereIn("message_id", results)
-//     .andWhere("user_id", userId);
-// }
-//
-// function emails(id) {
-//   return db("emails")
-//     .orderBy("date", "desc")
-//     .where("user_id", id);
-// }
-//
-// function updateEmail(userId, uid, changes) {
-//   return db("emails")
-//     .where("user_id", userId)
-//     .andWhere("uid", uid)
-//     .update(changes);
-// }
-//
-// function deleteEmail(uid) {
-//   return db("emails")
-//     .where("uid", uid)
-//     .del();
-// }
-//
-// function getLastEmailFromUser(userid) {
-//   return db("emails")
-//     .orderBy("uid", "desc")
-//     .where("user_id", userid)
-//     .first();
-// }
-//
-// function findEmailbyId(id) {
-//   return db("emails")
-//     .where({ id })
-//     .first();
-// }
-//
-// function addEmail(email) {
-//   return db("emails")
-//     .insert(email, "id")
-//     .then(ids => {
-//       const [id] = ids;
-//
-//       return findEmailbyId(id);
-//     });
-// }
-//
-// function getEmailIds(userId) {
-//   return db("emails")
-//     .select("uid")
-//     .where("user_id", userId);
-// }
-//
-// function deleteAllEmailsByUser(userId) {
-//   return db("emails")
-//     .where("user_id", userId)
-//     .del();
-// }
-//
-// function getTagsForMessage(messageId) {
-//   return db("tags")
-//     .select("tag")
-//     .where("email_id", messageId);
-// }
-//
-// function getMessageTagsFromUser(userId) {
-//   const messages = db("messages").where({ userid });
-//
-//   const newMessageArray = messages.map(message => {
-//     return get(message.id);
-//   });
-//   return newMessageArray;
-// }
-//
-// function get(messageId) {
-//   const messages = db("messages");
-//
-//   if (messageId) {
-//     messages.where({ messageId }).first();
-//
-//     const promises = [messages, getTagsForMessage(messageId)];
-//
-//     return Promise.all(promises).then(results => {
-//       const [message, tags] = results;
-//
-//       if (message) {
-//         message.tags = tags;
-//
-//         return message;
-//       } else {
-//         return null;
-//       }
-//     });
-//   }
-//   return messages;
-// }
+module.exports = {
+  getResults,
+  getLastEmailFromUser,
+  getEmailIds,
+  addEmail,
+  deleteAllEmailsByUser,
+  updateEmail,
+  deleteEmail,
+  getTagsForMessage,
+  getMessageTagsFromUser,
+  get,
+  findEmailbyId,
+  emails
+};
+
+function getResults(userId, results) {
+  // const numArray = results.map(num => {
+  //   return num * 1;
+  // });
+
+  return db("emails")
+    .whereIn("message_id", results)
+    .andWhere("user_id", userId);
+}
+
+function emails(id) {
+  return db("emails")
+    .orderBy("date", "desc")
+    .where("user_id", id);
+}
+
+function updateEmail(userId, uid, changes) {
+  return db("emails")
+    .where("user_id", userId)
+    .andWhere("uid", uid)
+    .update(changes);
+}
+
+function deleteEmail(uid) {
+  return db("emails")
+    .where("uid", uid)
+    .del();
+}
+
+function getLastEmailFromUser(userid) {
+  return db("emails")
+    .orderBy("uid", "desc")
+    .where("user_id", userid)
+    .first();
+}
+
+function findEmailbyId(id) {
+  return db("emails")
+    .where({ id })
+    .first();
+}
+
+function addEmail(email) {
+  return db("emails")
+    .insert(email, "id")
+    .then(ids => {
+      const [id] = ids;
+
+      return findEmailbyId(id);
+    });
+}
+
+function getEmailIds(userId) {
+  return db("emails")
+    .select("uid")
+    .where("user_id", userId);
+}
+
+function deleteAllEmailsByUser(userId) {
+  return db("emails")
+    .where("user_id", userId)
+    .del();
+}
+
+function getTagsForMessage(messageId) {
+  return db("tags")
+    .select("tag")
+    .where("email_id", messageId);
+}
+
+function getMessageTagsFromUser(userId) {
+  const messages = db("messages").where({ userid });
+
+  const newMessageArray = messages.map(message => {
+    return get(message.id);
+  });
+  return newMessageArray;
+}
+
+function get(messageId) {
+  const messages = db("messages");
+
+  if (messageId) {
+    messages.where({ messageId }).first();
+
+    const promises = [messages, getTagsForMessage(messageId)];
+
+    return Promise.all(promises).then(results => {
+      const [message, tags] = results;
+
+      if (message) {
+        message.tags = tags;
+
+        return message;
+      } else {
+        return null;
+      }
+    });
+  }
+  return messages;
+}

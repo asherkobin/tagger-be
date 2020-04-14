e9a3ab06958ad870cf69e7ffe62aedf3bac36807
ElkBranch <elggren.spencer@gmail.com>
2020-04-14 10:45:29 -0600

    getEmail now id now message_id. GetEmailList now follows label.

--- knexfile.js
+++ knexfile.js
@@ -28,7 +28,7 @@ module.exports = {
 
   production: {
     client: "pg",
-    connection: process.env.DATABASE_URL,
+    connection: 'postgresql://localhost' || process.env.DATABASE_URL,
     migrations: {
       tableName: "knex_migrations",
       directory: "./data/migrations"
